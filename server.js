02816ba7f357b674d2efcce9346264909456668e
TaimurHasan <taimurhasan11@gmail.com>
2022-04-30 10:54:42 -0400

    set up server.js file to serve index.html route to express server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// instantiate express for use in server side programming
+const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+
+// parse incoming JSON data
+app.use(express.json());
+
+// ensure files looked up relative to static directory
+app.use(express.static('public'));
+
+// routes
+app.use('/', htmlRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`Server now on port ${PORT}!`);
+})
