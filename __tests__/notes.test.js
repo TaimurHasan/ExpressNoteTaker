d8283ea83382179e5d6d6e0aaab43ba44947b816
TaimurHasan <taimurhasan11@gmail.com>
2022-05-05 19:35:56 -0400

    adding remaining tests to suite

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const { validateNote, createNewNote, filterOutId, pushFilteredArray } = require('../lib/notes');
 let db = require('../db/db.json');
+const { create } = require('domain');
 
 jest.mock('fs');
 
@@ -20,3 +21,36 @@ test('validates that a note contains required fields', () => {
     expect(validResult).toBe(true);
     expect(invalidResult).toBe(false); 
 });
+
+test('checks if function creates a new note', () => {
+    const note = {
+        title: 'test note',
+        text: 'this is a test note'
+    };
+
+    const newNote = createNewNote(note, db);
+
+    expect(newNote.title).toBe('test note');
+    expect(newNote.text).toBe('this is a test note');
+});
+
+test('check if id is filtered out correctly', () => {
+    const noteArray = 
+    [   
+        {
+            title: 'Note to delete',
+            text: 'Should not remain',
+            id: 1
+        },
+        {
+            title: 'Note to keep',
+            text: 'Should remain',
+            id: 2
+        }
+    ];
+
+    const result = filterOutId(1, noteArray);
+
+    expect(result.length).toBe(1);
+    expect(result[0].title).toBe('Note to keep');
+})
